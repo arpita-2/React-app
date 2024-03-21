1b5eb8f5db73cef2168dd3de5bd37f422a204365
arpita-2 <dasguptaarpita1999@gmail.com>
2024-03-21 14:45:15 +0530

    jsx learning

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import React from "react";
 import  ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id : "heading"}, "Hello world from React");
-//so basically the work of react is to create element which is we know is the core element
+//react.createElement => object => HTML element (rendering)
+//jSX .................JSX is just a syntax but react element is an object
+//jsx => react.createElement => Html element
 
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    //here we use reactdom because we have to put the element in the browser or we can say in the root
-    root.render(heading);
+const jsxHeading = <h1 id="heading">Hello world!</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
