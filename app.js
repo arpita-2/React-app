f68b3043068badeea5bdf4349516e6b201d11d51
arpita-2 <dasguptaarpita1999@gmail.com>
2024-03-22 15:21:24 +0530

    doing optional chaining to beautify

--- app.js
+++ app.js
@@ -1981,25 +1981,27 @@ const resList = [{
 //in src, we have done string concaetination
 
 /**
- * 
+ * bunch of data is given as resList
+ * now destructing the resData key to not write resData.data again and again by doing optional chaining
  * 
  */
 const RestaurantCard = (props) => {
     const {resData} = props;
+    const {cloudinaryImageId, name, cuisines, avgRating, costForTwo, deliveryTime} = resData?.data
     return (
     <div className="res-card">
         <img className="res-img"
         src={
             "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" +
-            resData.data.cloudinaryImageId
+            cloudinaryImageId
           }
     />
 
-    <h3>{resData.data.name}</h3>
-    <h4>{resData.data.cuisines}</h4>
-    <h4>{resData.data.avgRating}</h4>
-    <h4>{resData.data.costForTwo / 100}</h4>
-    <h4>{resData.data.deliveryTime} minutes</h4>
+    <h3>{name}</h3>
+    <h4>{cuisines.join(", ")}</h4>
+    <h4>{avgRating}</h4>
+    <h4>{costForTwo / 100}</h4>
+    <h4>{deliveryTime} minutes</h4>
     </div> 
     );  
 };
